7cbee2a167d92e9f6394255ac628bbd9d10b3cec
Etcheverry <juan.etcheverry@geopagos.local>
2023-01-18 20:37:00 -0300

    Entrega-Final

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -4,7 +4,7 @@ const Productos = () => {
     return (
         <div className="col-md-6 container-fluid d-flex align-items-start NavBar mt-2 mb-2">
         <img src={"/images/exclamation-triangle.svg"} alt={"Logo de exclamacion"} width={32}/>
-        <p>SITIO EN CONSTRUCCION</p>
+        <p>SITIO EN CONSTRUCCION - INAUGURACION PROXIMAMENTE</p>
         </div>
     )
 }
--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -9,10 +9,11 @@ const ThankYou = () => {
             <div className="row">
                 <div className="col-md-12 text-center">
                     <div className="alert alert-danger" role="alert">
-                        <h1>Gracias por tu Compra!</h1>
+                        <h1>Gracias por comprar en Aromas en el Alma!!</h1>
                         <p>Tu Número de Compra es: <b>{id}</b></p>
+                        <p> Te enviaremos las mejores promociones a tu correo electronico! te esperamos pronto.</p>
                     </div>
-                    <Link to={"/"} className="btn fondoNaranja">Volver a la Página Principal</Link>
+                    <Link to={"/"} className="btn fondoNaranja">Volver a la Página de Inicio</Link>
                 </div>
             </div>
         </div>
